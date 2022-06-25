37bc4c817371719b0401ac2eca2236193ddd7630
Jan Beckschewe <17655375+jan-be@users.noreply.github.com>
2022-06-25 17:33:57 +0200

    Add option to add video to real time decoding with second ffmpeg pipe

--- src/main.py
+++ src/main.py
@@ -1,33 +1,17 @@
+import subprocess as sp
 import sys
-import time
+import threading
 
-import ffmpeg
+import cv2
 import numpy as np
 
 import audio_decoder
 
-# loosely based on https://kkroening.github.io/ffmpeg-python/
 
-if __name__ == "__main__":
-    # in_filename = "rtsp://localhost/live"
-    in_filename = "tcp://localhost:1234"
-    # in_filename = "https://f111.rndfnk.com/ard/wdr/1live/diggi/mp3/128/stream.mp3?sid=2B1eCeSmJXOZEEiPpOnnykFYHP4&token=pGly4HjCjIPjybbqtozZnU3Oh1m-eP9YiXr5ya-83LI&tvf=zRZyqral-xZmMTExLnJuZGZuay5jb20"
-    # in_filename = "video.ts"
-
-    samplerate = 48000
-
-    process1 = (
-        ffmpeg
-        # .input(in_filename, rtsp_transport='tcp')
-        .input(in_filename)
-        .output('pipe:', format='s16le', ac=1, ar=samplerate)
-        .run_async(pipe_stdout=True)
-    )
-
+def audio(pipe):
     while True:
-        timee = time.time()
-        # maybe we should try to find some way to do this in a rolling fashion
-        in_bytes = process1.stdout.read(samplerate * 2 * 3)
+        in_bytes = pipe.read(samplerate * 2 * 3)
+
         if not in_bytes:
             break
 
@@ -37,6 +21,42 @@ if __name__ == "__main__":
         except Exception as err:
             print(err, file=sys.stderr)
 
-        print("time", time.time() - timee)
 
-    process1.wait()
+def video(pipe):
+    while True:
+        in_bytes = pipe.read(video_resolution[0] * video_resolution[1] * 3)
+
+        if not in_bytes or len(in_bytes) < video_resolution[0] * video_resolution[1] * 3:
+            break
+
+        # TODO replace with the code for detection
+        # img = np.frombuffer(in_bytes, np.uint8).reshape((video_resolution[0], video_resolution[1], 3))
+        # cv2.imshow("hmm", img)
+        # cv2.waitKey(0)
+
+
+# pipes based on https://stackoverflow.com/a/71057629
+
+if __name__ == "__main__":
+    # in_filename = "rtsp://localhost/live"
+    in_filename = "tcp://localhost:1234"
+    # in_filename = "https://f111.rndfnk.com/ard/wdr/1live/diggi/mp3/128/stream.mp3?sid=2B1eCeSmJXOZEEiPpOnnykFYHP4&token=pGly4HjCjIPjybbqtozZnU3Oh1m-eP9YiXr5ya-83LI&tvf=zRZyqral-xZmMTExLnJuZGZuay5jb20"
+    # in_filename = "video.ts"
+
+    samplerate = 48000
+    video_resolution = (720, 1280)
+    # video_resolution = (1080, 1920)
+
+    process = sp.Popen(
+        'ffmpeg -hide_banner -loglevel error '  # Set loglevel to error for disabling the prints ot stderr
+        # '-rtsp_transport tcp '              # needed when using obs over rtsp
+        f'-i {in_filename} '
+        '-vcodec rawvideo -pix_fmt rgb24 '  # Raw video codec with rgb24 pixel format                               
+        '-map 0:v -f:v rawvideo pipe:2 '  # rawvideo format is mapped to stderr pipe
+        f'-acodec pcm_s16le -ar {samplerate} -ac 1 '  # Audio codec pcm_s16le (-ar 16k has no affect)
+        '-map 0:a -f:a s16le pipe:1 '  # s16le audio format is mapped to stdout pipe
+        '-report',  # Create a log file (because we can't the statuses that are usually printed to stderr).
+        stdout=sp.PIPE, stderr=sp.PIPE)
+
+    threading.Thread(target=audio, args=(process.stdout,)).start()
+    threading.Thread(target=video, args=(process.stderr,)).start()
