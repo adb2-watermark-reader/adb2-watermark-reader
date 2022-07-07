cda57284fb4cb2e7ac5417f8012a353be1121e8f
Alexander Schmitz <alexander.schmitz@tu-berlin.de>
2022-07-07 19:08:14 +0200

    video real time decoding

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import subprocess as sp
 import sys
 import threading
+from tracemalloc import get_object_traceback
 from typing import IO
 
 import cv2
@@ -8,6 +9,7 @@ import numpy as np
 import audio_spec_constants as consts
 
 import audio_decoder
+import video_decoder
 
 
 def audio(pipe: IO):
@@ -37,7 +39,15 @@ def video(pipe: IO):
             break
 
         # TODO replace with the code for detection
-        # img = np.frombuffer(in_bytes, np.uint8).reshape((video_resolution[0], video_resolution[1], 3))
+        img = np.frombuffer(in_bytes, np.uint8).reshape((video_resolution[0], video_resolution[1], 3))
+
+        try:
+            video_payload = video_decoder.decode(img)
+            if not video_payload == None:
+                print(video_payload)
+        except Exception as err:
+            print(err, file=sys.stderr)
+
         # cv2.imshow("hmm", img)
         # cv2.waitKey(0)
 
@@ -63,7 +73,7 @@ if __name__ == "__main__":
         f'-acodec pcm_s16le -ar {samplerate} -ac 1 '  # Audio codec pcm_s16le (-ar 16k has no affect)
         '-map 0:a -f:a s16le pipe:1 '  # s16le audio format is mapped to stdout pipe
         # '-report'  # Create a log file (because we can't the statuses that are usually printed to stderr).
-        , stdout=sp.PIPE, stderr=sp.PIPE)
+        , stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
 
     threading.Thread(target=audio, args=(process.stdout,)).start()
     threading.Thread(target=video, args=(process.stderr,)).start()
